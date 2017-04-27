7620ab866d1fb3510f797b7f760b5c24cb1492a2
Arash <arash.shakery@gmail.com>
2017-04-27 18:45:35 +0430

    10365

---
+++ UVA/vol-103/10365.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);
+
+    vector<int> factors[1001];
+    for (int i=1; i<1001; ++i)
+        for (int j=1; j<=i; ++j)
+            if (i % j == 0)
+                factors[i].push_back(j);
+
+    int T, n;
+    cin >> T;
+    while (T-- && cin>>n) {
+        int ms = (int)1e9;
+        for (int i: factors[n])
+            for (int j: factors[n/i]) {
+                int k = (n/i)/j;
+                ms = min(ms, (i*j + j*k + k*i)<<1);
+            }
+        cout << ms << "\n";
+    }
+}
