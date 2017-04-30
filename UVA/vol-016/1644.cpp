e39e80ced62b53bcfcc4d9e5d96b2a6d0e4b38aa
Arash <arash.shakery@gmail.com>
2017-04-30 14:02:38 +0430

    1644

---
+++ UVA/vol-016/1644.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isnp[1299710];
+int primes[100143], ins[1299710], pnt;
+void sieve() {
+    isnp[0] = isnp[1] = 1;
+    for (int i=2; i<=1299709; ++i)
+        if (!isnp[i]) {
+            primes[pnt++] = i;
+            for (int j=i<<1; j<=1299709; j+=i)
+                isnp[j] = 1;
+        }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);
+
+    sieve();
+    for (int j=2, k=-1; j<=1299709; ++j) {
+        if (!isnp[j]) ++k;
+        ins[j] = k;
+    }
+
+    int n;
+    while (cin >> n && n)
+        if (!isnp[n]) cout << "0\n";
+        else {
+            int ind = ins[n];
+            cout << primes[ind+1]-primes[ind] << '\n';
+        }
+}
