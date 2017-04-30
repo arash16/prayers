f390cc095f5e2d448b833832a3a5b128b48443e6
Arash <arash.shakery@gmail.com>
2017-04-30 09:27:53 +0430

    12626

---
+++ UVA/vol-126/12626.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+char SS[] = "MARGIT";
+int CC[] = {1,3,2,1,1,1};
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);
+
+    int T;
+    cin >> T;
+    string str;
+    while (T-- && cin >> str) {
+        int C[91] = {};
+        for (int i=0; str[i]; ++i)
+            if (str[i]<91)
+                ++C[str[i]];
+
+        int mx = (int)1e9;
+        for (int i=0; SS[i]; ++i)
+            mx = min(mx, C[SS[i]]/CC[i]);
+        cout << mx << '\n';
+    }
+}
