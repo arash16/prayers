6e256d57be25f43ba032f7fa5239b0e6e1f99c8f
Arash <arash.shakery@gmail.com>
2017-05-03 10:34:02 +0430

    10585

---
+++ UVA/vol-105/10585.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Point {
+    int x, y;
+    bool operator < (const Point &o) const {
+        return x==o.x ? y<o.y : x<o.x;
+    }
+}
+P[10143], pp;
+
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);
+
+    int T, n;
+    cin >> T;
+    while (T-- && cin >> n) {
+        long long sx = 0, sy = 0;
+        for (int i=0; i<n; ++i) {
+            cin >> P[i].x >> P[i].y;
+            sx += P[i].x;
+            sy += P[i].y;
+        }
+
+        bool result = 0;
+        if ((2*sx)%n==0 && (2*sy)%n==0) {
+            long long tx = 2*sx/n, ty = 2*sy/n;
+            sort(P, P+n);
+            result = 1;
+            for (int i=0; result && i<n; ++i) {
+                pp.x = tx - P[i].x;
+                pp.y = ty - P[i].y;
+                result &= binary_search(P, P+n, pp);
+            }
+        }
+
+        cout << (result ? "yes\n" : "no\n");
+    }
+}
