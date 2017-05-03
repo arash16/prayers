a000045797e9d30c54770a2d52011f44d2ede4b3
Arash <arash.shakery@gmail.com>
2017-05-03 09:58:52 +0430

    10466

---
+++ UVA/vol-104/10466.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const double PI = acos(-1);
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    cout << fixed << setprecision(4);
+
+    int n, t, ri, ti;
+    while (cin >> n >> t) {
+        double x = 0, y = 0;
+        for (int i=0; i<n; ++i) {
+            cin >> ri >> ti;
+            double deg = 2*PI*double(t % ti) / ti;
+            x += ri * cos(deg);
+            y += ri * sin(deg);
+
+            if (i) cout << ' ';
+            cout << sqrt(x*x + y*y);
+        }
+        cout << '\n';
+    }
+}
