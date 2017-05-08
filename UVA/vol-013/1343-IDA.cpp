86455d1ababeda068b427d05fa9ca0b721c05fbf
Arash <arash.shakery@gmail.com>
2017-05-08 09:45:20 +0430

    1343

---
+++ UVA/vol-013/1343-IDA.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int moves[8][7] = {
+    { 0, 2, 6, 11, 15, 20, 22 },
+    { 1, 3, 8, 12, 17, 21, 23 },
+    { 10, 9, 8, 7, 6, 5, 4 },
+    { 19, 18, 17, 16, 15, 14, 13 },
+    { 23, 21, 17, 12, 8, 3, 1 },
+    { 22, 20, 15, 11, 6, 2, 0 },
+    { 13, 14, 15, 16, 17, 18, 19 },
+    { 4, 5, 6, 7, 8, 9, 10}
+};
+int center[] = {6, 7, 8, 11, 12, 15, 16, 17};
+
+void moveForward(char XX[], int k) {
+    char t = XX[moves[k][0]];
+    for (int j=0; j<6; ++j)
+        XX[moves[k][j]] = XX[moves[k][j+1]];
+    XX[moves[k][6]] = t;
+}
+
+int H(char X[]) {
+    int cnt[3] = {};
+    for (int i=0; i<8; ++i)
+        ++cnt[X[center[i]]];
+    return 8 - max(cnt[0], max(cnt[1], cnt[2]));
+}
+
+
+int path[20], maxd;
+bool IDA (char X[], int d) {
+    int hv = H(X); // minimum remaining moves
+    if (!hv) {
+        if (!d) cout << "No moves needed\n";
+        else {
+            for (int i=0; i<d; ++i)
+                cout << char('A' + path[i]);
+            cout << '\n';
+        }
+        cout << X[6]+1 << '\n';
+        return 1;
+    }
+
+    if (d + hv > maxd) return 0;
+
+    char Y[24];
+    for (int k=0; k<8; ++k) {
+        path[d] = k;
+        memcpy(Y, X, 24);
+        moveForward(Y, k);
+        if (IDA(Y, d+1))
+            return 1;
+    }
+    return 0;
+}
+
+
+int main() {
+    int xx[24];
+    char X[24];
+    for (int cse=1; cin>>xx[0] && xx[0]; ++cse) {
+        for (int i=1; i<24; ++i)
+            cin >> xx[i];
+
+        for (int i=0; i<24; ++i)
+            X[i] = xx[i] - 1;
+
+        bool solved = 0;
+        for (maxd=1; !solved; ++maxd)
+            solved |= IDA(X, 0);
+    }
+}
---
+++ UVA/vol-013/1343-bfs-TLE.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int moves[8][7] = {
+    { 0, 2, 6, 11, 15, 20, 22 },
+    { 1, 3, 8, 12, 17, 21, 23 },
+    { 10, 9, 8, 7, 6, 5, 4 },
+    { 19, 18, 17, 16, 15, 14, 13 },
+    { 23, 21, 17, 12, 8, 3, 1 },
+    { 22, 20, 15, 11, 6, 2, 0 },
+    { 13, 14, 15, 16, 17, 18, 19 },
+    { 4, 5, 6, 7, 8, 9, 10}
+};
+
+#define MAXS 1048576
+int TP[MAXS] = {1, 2, 3}, sz;
+bool X[MAXS][24];
+
+// 235968
+int finY = (1 <<  6) | (1 <<  7)
+         | (1 <<  8) | (1 << 11)
+         | (1 << 12) | (1 << 15)
+         | (1 << 16) | (1 << 17);
+bool isFin(int state) { return (state&finY) == finY; }
+
+int calcY(const bool XX[]) {
+    int r = 0;
+    for (int i=0; i<24; ++i)
+        r = (r<<1) | XX[i];
+    return r;
+}
+
+void moveForward(bool XX[], int k) {
+    bool t = XX[moves[k][0]];
+    for (int j=0; j<6; ++j)
+        XX[moves[k][j]] = XX[moves[k][j+1]];
+    XX[moves[k][6]] = t;
+}
+
+void moveBackward(bool XX[], int k) {
+    bool t = XX[moves[k][6]];
+    for (int j=6; j>0; --j)
+        XX[moves[k][j]] = XX[moves[k][j-1]];
+    XX[moves[k][0]] = t;
+}
+
+
+int S[1<<24], P[1<<24];
+string BFS(int cse) {
+    for (sz = 0; sz < 3; ++sz) {
+        int state = calcY(X[sz]);
+        if (isFin(state))
+            return "No moves needed";
+        S[state] = cse;
+    }
+
+    queue<int> q, qd;
+    q.push(3);
+    qd.push(0);
+
+    for (int s=0; !q.empty();) {
+        int l = q.front(); q.pop();
+        int d = qd.front(); qd.pop();
+
+        for (int k=0; k<8; ++k) {
+            for (int i=s; i<l; ++i) {
+                bool *XX = X[sz];
+                memcpy(XX, X[i], 24);
+                moveForward(XX, k);
+                int state = calcY(XX);
+
+                if (S[state]!= cse) {
+                    S[state] = cse;
+                    P[state] = k;
+                    TP[sz] = TP[i];
+                    if (isFin(state)) {
+                        string result;
+                        for (int j=0; j<=d; ++j) {
+                            int state = calcY(XX);
+                            result = char('A' + P[state]) + result;
+                            moveBackward(XX, P[state]);
+                        }
+                        return result;
+                    }
+                    ++sz;
+                }
+            }
+            q.push(sz);
+            qd.push(d+1);
+        }
+        s = l;
+    }
+    return "";
+}
+
+
+int main() {
+    int xx[24];
+    for (int cse=1; cin>>xx[0] && xx[0]; ++cse) {
+        for (int i=1; i<24; ++i)
+            cin >> xx[i];
+
+        for (int i=0; i<24; ++i) {
+            X[0][i] = xx[i] == 1;
+            X[1][i] = xx[i] == 2;
+            X[2][i] = xx[i] == 3;
+        }
+
+        cout << BFS(cse) << '\n';
+        cout << TP[sz] << '\n';
+    }
+}
