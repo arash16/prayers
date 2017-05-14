87e8390f045d8c8b72f73a383eaa51bcbfab6ec4
Arash <arash.shakery@gmail.com>
2017-05-14 10:12:41 +0430

    11615

---
+++ UVA/vol-116/11615.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int lg2(int x) {
+    int r = 0;
+    for (; x; x>>=1)
+        ++r;
+    return r-1;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);
+
+    int T, n, a, b;
+    cin >> T;
+    while (T-- && cin >> n >> a >> b) {
+        int al = (1<<n) - 1,
+            gr = n - lg2(max(a, b)),
+            ar = (1<<(gr)) - 2;
+        cout << al - ar << '\n';
+    }
+}
