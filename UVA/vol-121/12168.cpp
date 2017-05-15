01fe111879cbf1e3ad76dc33c5fd4baa89bd4e01
Arash <arash.shakery@gmail.com>
2017-05-15 13:00:53 +0430

    12168

---
+++ UVA/vol-121/12168.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> adj[555];
+int match[555];
+bool seen[555];
+bool bpm(int u) {
+    for (int v: adj[u])
+        if (!seen[v]) {
+            seen[v] = 1;
+            if (match[v]==-1 || bpm(match[v])) {
+                match[v] = u;
+                return true;
+            }
+        }
+    return false;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);
+
+    string X[555], Y[555];
+    int T, c, d, n;
+    cin >> T;
+    while (T-- && cin >> c >> d >> n) {
+        vector<int> L, R;
+        for (int i=0; i<n; ++i) {
+            adj[i].clear();
+            match[i] = -1;
+
+            cin >> X[i] >> Y[i];
+            if (X[i][0] == 'C')
+                L.push_back(i);
+            else
+                R.push_back(i);
+        }
+
+        for (int i=0; i<L.size(); ++i)
+            for (int j=0; j<R.size(); ++j)
+                if (X[L[i]]==Y[R[j]] || Y[L[i]]==X[R[j]])
+                    adj[i].push_back(j);
+
+        int cnt = 0;
+        for (int i=0; i<L.size(); ++i) {
+            memset(seen, 0, R.size());
+            if (bpm(i)) ++cnt;
+        }
+        cout << (n-cnt) << '\n';
+    }
+}
