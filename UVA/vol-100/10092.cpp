6a7b8764b1265229e75fd0cc64e4a178aeeb54c9
Arash <arash.shakery@gmail.com>
2017-05-16 13:40:51 +0430

    10092

---
+++ UVA/vol-100/10092.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+vector<int> adj[23];
+int matchR[1024];
+bool matchL[23][1024], seen[1024];
+bool bpm(int u) {
+    for (int v: adj[u])
+        if (!seen[v]) {
+            seen[v] = 1;
+            if (!matchR[v] || bpm(matchR[v])) {
+                if (matchR[v])
+                    matchL[matchR[v]][v] = 0;
+                matchL[matchR[v] = u][v] = 1;
+                return true;
+            }
+        }
+    return false;
+}
+
+
+
+char BUF[1000000];
+int main() {
+    cout.rdbuf()->pubsetbuf(BUF, 1000000);
+    ios_base::sync_with_stdio(0);cin.tie(0);
+
+    int C[23], n, p;
+    while (cin >> n >> p && (n||p)) {
+        for (int i=1; i<=n; ++i) {
+            memset(matchL[i], 0, p);
+            adj[i].clear();
+            cin >> C[i];
+        }
+
+        for (int i=0, sz,u; i<p; ++i) {
+            matchR[i] = 0;
+            cin >> sz;
+            while (sz--) {
+                cin >> u;
+                adj[u].push_back(i);
+            }
+        }
+
+        for (int i=1; i<=n; ++i)
+            for (int j=0; j<C[i]; ++j) {
+                memset(seen, 0, p);
+                if (!bpm(i)) {
+                    cout << "0\n";
+                    goto fin;
+                }
+            }
+
+        cout << "1\n";
+        for (int i=1; i<=n; ++i) {
+            bool f=1;
+            for (int j=0; j<p; ++j)
+                if (matchL[i][j]) {
+                    if (f) f = 0;
+                    else cout << ' ';
+                    cout << j+1;
+                }
+            cout << '\n';
+        }
+        fin:;
+    }
+}
