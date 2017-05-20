95f7046a1baf760c0ba54b4c8a3f5ab9389141be
Arash <arash.shakery@gmail.com>
2017-05-20 11:40:58 +0430

    11432

---
+++ UVA/vol-114/11432.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);
+
+    long long DP[34][77][77] = {};
+    for (int g=1; g<34; ++g) {
+        DP[g][0][0] = 1;
+        for (int i=1; i<=66; ++i) {
+            for (int j=1; j<=i; ++j) {
+                DP[g][i][j] = 0;
+                for (int k=min(i, g); k>0; --k)
+                    DP[g][i][j] += DP[g][i-k][i-j];
+            }
+        }
+    }
+
+    for (int cse=1, d,g; cin >> d >> g && (d>=0 && g>=0); ++cse) {
+        long long sum = 0; d <<= 1;
+        for (int i=1; d-i>=0; i+=g+1)
+            sum += DP[g][d-i][d>>1];
+
+        cout << "Case " << cse << ": " << 2*sum << endl;
+    }
+}
