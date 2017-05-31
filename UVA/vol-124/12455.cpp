6877ae80a0adc79311f7b602531709e73414501e
Arash <arash.shakery@gmail.com>
2017-05-31 13:46:51 +0430

    12455

---
+++ UVA/vol-124/12455.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);
+
+    int X[23], T, n, l;
+    cin >> T;
+    while (T-- && cin>>l>>n) {
+        int sz = 0;
+        for (int i=0; i<n; ++i) {
+            cin >> X[sz];
+            if (X[sz] <= l) ++sz;
+        }
+
+        bool DP[1001] = { 1 };
+        for (int i=0; i<sz; ++i)
+            for (int j=l-X[i]; j>=0; --j)
+                DP[j + X[i]] |= DP[j];
+
+        cout << (DP[l] ? "YES\n" : "NO\n");
+    }
+}
