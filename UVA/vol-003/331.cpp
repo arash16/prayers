92ee08beb3fb93be9ea04bd0b798082f05ea145f
Arash <arash.shakery@gmail.com>
2017-06-01 16:49:07 +0430

    331

---
+++ UVA/vol-003/331.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int X[7], n, r, best;
+void bt(int cnt) {
+    if (cnt > best) return;
+
+    bool sorted = 1;
+    for (int i=1; i<n; ++i)
+        if (X[i] < X[i-1]) {
+            swap(X[i], X[i-1]);
+            bt(cnt + 1);
+            swap(X[i], X[i-1]);
+            sorted = 0;
+        }
+
+    if (sorted) {
+        if (cnt < best) r = 0;
+        best = cnt; ++r;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);
+
+    for (int cse=1; cin >> n && n; ++cse) {
+        for (int i=0; i<n; ++i)
+            cin >> X[i];
+
+        best = n*(n-1)/2;
+        r = 0;
+        bt(0);
+        cout << "There are " << (!best ? 0 : r) << " swap maps for input data set " << cse << ".\n";
+    }
+}
