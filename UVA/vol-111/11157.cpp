54082f86f09cf1833ac136bca70db9baac3fbca3
Arash <arash.shakery@gmail.com>
2017-06-02 16:45:33 +0430

    11157

---
+++ UVA/vol-111/11157.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);
+
+    char tp[143], ch;
+    int X[143]={}, T, n, d;
+    cin >> T;
+    for (int cse=1; cse<=T; ++cse) {
+        cin >> n >> d;
+
+        int sz = 1;
+        for (int i=0; i<n; ++i) {
+            cin >> tp[sz] >> ch >> X[sz];
+            if (X[i] < d) ++sz;
+        }
+        tp[0] = tp[sz] = 'B';
+        X[sz++] = d;
+
+        int lx=0, mxd=0;
+        bool used[101] = {};
+        for (int i=0; i<sz; ++i) {
+            if (tp[i] == 'S') ++i;
+            if (tp[i] == 'S') used[i] = 1;
+            mxd = max(mxd, X[i]-lx);
+            lx = X[i];
+        }
+
+        for (int i=sz-1; i>=0; --i)
+            if (!used[i]) {
+                mxd = max(mxd, lx-X[i]);
+                lx = X[i];
+            }
+
+        cout << "Case " << cse << ": " << mxd << '\n';
+    }
+}
