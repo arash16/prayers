605e0e6291b9bb6856a5176c54e600154004fbd3
Arash <arash.shakery@gmail.com>
2017-06-03 00:15:54 +0430

    10128

---
+++ UVA/vol-101/10128.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+long long DP[14][14][14];
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);
+
+    DP[1][1][1] = 1;
+    for (int n=2; n<=13; ++n)
+        for (int l=1; l<=n; ++l)
+            for (int r=1; r<=n; ++r)
+                DP[n][l][r] = (n-2)*DP[n-1][l][r] + DP[n-1][l-1][r] + DP[n-1][l][r-1];
+
+    int T, n, l, r;
+    cin >> T;
+    while (T-- && cin >> n >> l >> r)
+        cout << DP[n][l][r] << '\n';
+}
