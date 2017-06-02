08c23fceb2afc4cdf660da610ae2c09bc9270b7c
Arash <arash.shakery@gmail.com>
2017-06-02 17:02:30 +0430

    11054

---
+++ UVA/vol-110/11054.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+inline int readchar(bool skip=true) {
+    const int N = 4194304;
+    static char buf[N];
+    static char *p = buf, *end = buf;
+    static bool fin = 0;
+    if (fin) return EOF;
+    if (p == end) {
+        if ((end = buf + fread(buf, 1, N, stdin)) == buf) {
+            fin = 1;
+            return EOF;
+        }
+        p = buf;
+    }
+    int result = *p;
+    if (skip) ++p;
+    return result;
+}
+
+inline bool isdigit(char ch) { return ch>='0' && ch<='9'; }
+inline long long readInt() {
+    char ch;
+    long long sgn=1, r=0;
+    while (!isdigit(ch=readchar()) && ch!='-')
+        if (ch == EOF) return EOF;
+    if (ch == '-') sgn = -1;
+    else r = ch-'0';
+    while (isdigit(ch=readchar()))
+        r = (r<<3) + (r<<1) + ch-'0';
+    return sgn*r;
+}
+
+// ---------------------------------------------------------------------------
+
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);
+
+    int n;
+    while ((n=readInt()) > 0) {
+        long long b = readInt(), s = 0;
+        for (int i=1; i<n; ++i) {
+            s += abs(b);
+            b += readInt();
+        }
+        cout << s << '\n';
+    }
+}
