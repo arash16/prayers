2eed9a57f3aa157ae1079f4085fc248dbef01457
Arash <arash.shakery@gmail.com>
2017-06-03 14:04:54 +0430

    311

---
+++ UVA/vol-003/311.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);
+
+    int C2[] = { 0, 5, 3, 1 },
+        C1[] = { 0, 7, 6, 5 },
+        x1, x2, x3, x4, x5, x6;
+    while (cin>>x1>>x2>>x3>>x4>>x5>>x6 && (x1||x2||x3||x4||x5||x6)) {
+        int cnt = x6 + x5 + x4 + ceil(x3/4.0);
+        x1 -= 11*x5 + C1[x3%4];
+        x2 -=  5*x4 + C2[x3%4];
+        if (x2 > 0) {
+            cnt += ceil(x2 / 9.0);
+            x2 -= 9 * ceil(x2 / 9.0);
+        }
+        if (x2 < 0) {
+            x1 -= -x2*4;
+            x2 = 0;
+        }
+
+        if (x1 > 0)
+            cnt += ceil(x1/36.0);
+
+        cout << cnt << '\n';
+    }
+}
