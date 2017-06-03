fd1352a6b1a30596b5e94d1333fd4ba9d3ef5ccd
Arash <arash.shakery@gmail.com>
2017-06-03 14:48:55 +0430

    668

---
+++ UVA/vol-006/668.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);
+
+    int T, n;
+    cin >> T;
+    while (T-- && cin>>n) {
+        int res[500]={}, sz=0, i;
+        for (i=2; i<=n; ++i)
+            res[sz++] = i,
+            n -= i;
+
+        if (n > 0) {
+            int k = i-n-2;
+            if (k < 0) k = 0;
+            res[sz++] = n + res[k];
+            res[k] = 0;
+        }
+
+        i=0;
+        while (!res[i])++i;
+        cout << res[i];
+        for (++i; i<sz; ++i)
+            if (res[i])
+                cout << ' ' << res[i];
+        cout << '\n';
+        if (T) cout << '\n';
+    }
+}
