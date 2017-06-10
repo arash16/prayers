2082bcfabd2449eebb7dc588268994358aaced47
Arash <arash.shakery@gmail.com>
2017-06-10 16:06:31 +0430

    11961

---
+++ UVA/vol-119/11961.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+vector<string> res;
+char E[] = "ACGT", str[17];
+void dfs(int idx, int cnt) {
+    if (!str[idx] || !cnt) {
+        res.push_back(str);
+        return;
+    }
+
+    char o = str[idx];
+    for (int i=0; i<4; ++i) {
+        str[idx] = E[i];
+        dfs(idx+1, cnt - (E[i] != o));
+    }
+    str[idx] = o;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);
+
+    int T, n, c;
+    cin >> T;
+    while (T-- && cin >> n >> c >> str) {
+        res.clear();
+        dfs(0, c);
+        cout << res.size() << '\n';
+        for (const string &s: res)
+            cout << s << '\n';
+    }
+}
