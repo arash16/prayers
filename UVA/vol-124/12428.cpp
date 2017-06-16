db45b46e861d9a6fd0249960155a411317fb7add
Arash <arash.shakery@gmail.com>
2017-06-16 17:39:38 +0430

    12428

---
+++ UVA/vol-124/12428.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+bool check(long long n, long long m) {
+    return m>=n-1 && m<=(n*(n-1)>>1);
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);
+
+    int T;
+    cin >> T;
+    while (T--) {
+        long long n, m;
+        cin >> n >> m;
+        int lo=0, hi=n-1;
+        while (lo <= hi) {
+            int mid = (lo + hi) >> 1;
+            if (check(n-mid, m-mid))
+                lo = mid + 1;
+            else
+                hi = mid - 1;
+        }
+        cout << lo-1 << '\n';
+    }
+}
