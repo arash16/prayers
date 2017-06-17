fab70b39a05e41f7f587c9a1ee1d12c136e47783
Arash <arash.shakery@gmail.com>
2017-06-18 03:59:43 +0430

    12843

---
+++ UVA/vol-128/12843.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int Ri[10000000], Rk[10000000];
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);
+
+    long long n, tn, sn, j, k2, k;
+    int T;
+    cin >> T;
+    for (int cse=1; cse<=T; ++cse) {
+        cin >> n;
+        tn = n<<1;
+        sn = sqrt(tn);
+
+        int sz = 0;
+        for (int i=1; i<=sn; ++i)
+            if (tn % i == 0) {
+                j = tn / i;
+                k = sqrt(k2 = i*i + j*j);
+                if (k*k == k2)
+                    Ri[sz] = i,
+                    Rk[sz] = k,
+                    ++sz;
+            }
+        cout << "Case " << cse << ": " << sz << "\n";
+        for (int i=0; i<sz; ++i)
+            cout << "(" << Ri[i] << ',' << (tn/Ri[i]) << ',' << Rk[i] << ")\n";
+    }
+}
