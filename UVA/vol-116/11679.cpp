b15db77cb09d2d8c53ebc8fc0544c03bc0328d76
Arash <arash.shakery@gmail.com>
2017-06-21 00:52:58 +0430

    11679

---
+++ UVA/vol-116/11679.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);
+
+    int R[23], n, m;
+    while (cin >> n >> m && (n||m)) {
+        for (int i=1; i<=n; ++i)
+            cin >> R[i];
+
+        for (int i=0, u,v,d; i<m; ++i) {
+            cin >> u >> v >> d;
+            R[u] -= d;
+            R[v] += d;
+        }
+
+        bool possible = 1;
+        for (int i=1; possible && i<=n; ++i)
+            possible &= R[i] >= 0;
+
+        cout << (possible ? "S\n" : "N\n");
+    }
+}
