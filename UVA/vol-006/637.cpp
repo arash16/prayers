9bb571382063a99333648d15076390f599712fe8
Arash <arash.shakery@gmail.com>
2017-08-14 00:54:26 +0430

    637

---
+++ UVA/vol-006/637.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void printPage(int p) {
+    if (p) cout << p;
+    else cout << "Blank";
+}
+
+int main() {
+    int n;
+    while (cin >> n && n) {
+        printf("Printing order for %d pages:\n", n);
+
+        int P[143][4] = {},
+            sz = (n+3)/4,
+            p=0, i;
+
+        for (i=0; i<sz; ++i)
+            P[i][0] = ++p,
+            P[i][1] = ++p;
+
+        for (--i; p<n; --i)
+            P[i][2] = ++p,
+            P[i][3] = ++p;
+
+        for (int i=0; i<sz; ++i) {
+            for (int j=0; j<4; ++j)
+                if (P[i][j] > n)
+                    P[i][j] = 0;
+
+            if (P[i][3] || P[i][0]) {
+                printf("Sheet %d, front: ", i+1);
+                printPage(P[i][3]);
+                cout << ", ";
+                printPage(P[i][0]);
+                cout << endl;
+            }
+
+            if (P[i][1] || P[i][2]) {
+                printf("Sheet %d, back : ", i+1);
+                printPage(P[i][1]);
+                cout << ", ";
+                printPage(P[i][2]);
+                cout << endl;
+            }
+        }
+    }
+}
