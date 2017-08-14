7717b840de1e1929a42f540a32efc480fe767094
Arash <arash.shakery@gmail.com>
2017-08-14 14:20:40 +0430

    12478

---
+++ UVA/vol-124/12478.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+char G[10][10] = {
+    "OBIDAIBKR",
+    "RKAULHISP",
+    "SADIYANNO",
+    "HEISAWHIA",
+    "IRAKIBULS",
+    "MFBINTRNO",
+    "UTOYZIFAH",
+    "LEBSYNUNE",
+    "EMOTIONAL"
+},
+names[10][10] = {
+    "RAKIBUL",
+    "ANINDYA",
+    "MOSHIUR",
+    "SHIPLU",
+    "OBAIDA",
+    "KABIR",
+    "SUNNY",
+    "WASI"
+};
+
+bool match(int ii, int jj, int F[], int l) {
+    if (9-ii >= l) {
+        int C[100] = {};
+        for (int i=0; i<l; ++i)
+            ++C[G[ii+i][jj]];
+        if (memcmp(C, F, sizeof(C)) == 0)
+            return 1;
+    }
+    if (9-jj >= l) {
+        int C[100] = {};
+        for (int j=0; j<l; ++j)
+            ++C[G[ii][jj+j]];
+        if (memcmp(C, F, sizeof(C)) == 0)
+            return 1;
+    }
+    return 0;
+}
+
+
+bool check(char s[]) {
+    int F[100] = {}, l;
+    for (l=0; s[l]; ++l)
+        ++F[s[l]];
+
+    int cnt = 0;
+    for (int i=0; i<9; ++i)
+        for (int j=0; j<9; ++j)
+            if (match(i, j, F, l) && ++cnt == 2)
+                return 1;
+    return 0;
+}
+
+
+
+int main() {
+    for (int i=0; i<8; ++i)
+        if (check(names[i])) {
+            cout << names[i] << endl;
+            return 0;
+        }
+}
