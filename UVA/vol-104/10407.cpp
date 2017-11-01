376ecf9130723e4d80fc784bf616a3409297e6da
Arash <arash.shakery@gmail.com>
2017-11-01 16:46:24 +0330

    10407

--- UVA/vol-104/10407.cpp
+++ UVA/vol-104/10407.cpp
@@ -9,11 +9,11 @@ int gcd(int a, int b) {
 int main() {
     int a, b;
     while (cin>>a && a) {
-      int rs = 0;
-      while (cin>>b && b)
-        if (!rs) rs = fabs(a-b);
-        else if (a-b)
-      rs = gcd(rs, fabs(a-b));
-      cout << rs << endl;
+        int rs = 0;
+        while (cin>>b && b)
+            if (!rs) rs = fabs(a-b);
+            else if (a-b)
+                rs = gcd(rs, fabs(a-b));
+        cout << rs << endl;
     }
 }
