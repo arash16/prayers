3bba3aba2779b72970ca9460b32857c2b37d2407
Arash <arash.shakery@gmail.com>
2017-11-01 19:28:29 +0330

    701

---
+++ UVA/vol-007/701.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    double c = log2(10), lg2n, lg2n1, left, right;
+
+    int n;
+    while (cin >> n) {
+        lg2n = log2(n);
+        lg2n1= log2(n+1);
+
+        for (int len = log10(n)+2; ; ++len) {
+            left = lg2n + len * c;
+            right= lg2n1+ len * c;
+            if (int(left) < int(right)) {
+                cout << (int)ceil(left) << endl;
+                break;
+            }
+        }
+    }
+}
