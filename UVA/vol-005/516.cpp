8bdd95ff1f3591ea348ae42729c1ecf1d7a371ef
Arash <arash.shakery@gmail.com>
2017-12-25 20:17:55 +0330

    516

---
+++ UVA/vol-005/516.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);
+
+    string line;
+    while (getline(cin, line) && line!="0") {
+        stringstream sin(line);
+        int x = 1, p, e;
+        while (sin >> p >> e)
+            x *= pow(p, e);
+
+        --x;
+        vector<int> vec;
+        for (int i=2, sq=sqrt(x); i<=sq; ++i)
+            if (x % i == 0) {
+                int e = 0;
+                while (x % i == 0) {
+                    x /= i;
+                    ++e;
+                }
+
+                vec.push_back(e);
+                vec.push_back(i);
+                sq = sqrt(x);
+            }
+        if (x > 1) {
+            vec.push_back(1);
+            vec.push_back(x);
+        }
+
+        cout << vec[vec.size()-1];
+        for (int i=vec.size()-2; i>=0; --i)
+            cout << ' ' << vec[i];
+        cout << endl;
+    }
+}
