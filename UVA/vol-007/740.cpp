624a0904dd1a830afe1471ef43571c58fb1dbec9
Arash <arash.shakery@gmail.com>
2017-12-25 22:36:55 +0330

    740

---
+++ UVA/vol-007/740.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);
+
+    char sd[100], su[100];
+    cin.getline(sd, 100);
+    cin.getline(su, 100);
+
+    string str;
+    while (cin >> str) {
+        char *ss = sd;
+
+        for (int i=0; i<str.length(); i+=5) {
+            int c = 0;
+            for (int j=0; j<5; ++j)
+                c = (c<<1) | (str[i+j] - '0');
+
+            if (c == 31) ss = su;
+            else if (c == 27) ss = sd;
+            else cout << ss[c];
+        }
+        cout << endl;
+    }
+}
