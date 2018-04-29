2886a1b9a441bebb3bcb479f56b3d6aecfb987cf
Arash <arash.shakery@gmail.com>
2018-04-29 13:52:59 +0430

    10820

---
+++ UVA/vol-108/10820.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+long long phi[50005];
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);
+
+    for (int i=1; i<50005; i++)
+        phi[i] = i;
+    for (int p=2; p<50005; ++p)
+        if (phi[p] == p) {
+            phi[p] = p-1;
+            for (int i=2*p; i<50005; i += p)
+                phi[i] = (phi[i]/p) * (p-1);
+        }
+
+    phi[1] = 1;
+    for (int p=2; p<50005; ++p)
+        phi[p] = phi[p]*2 + phi[p-1];
+
+    int n;
+    while (cin >> n && n)
+        cout << phi[n] << endl;
+}
