8bd51ba2d142bd02c603939b5beba5192a25ebd3
Arash <arash.shakery@gmail.com>
2018-05-01 15:46:03 +0430

    10646

---
+++ UVA/vol-106/10646.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);
+
+    int T;
+    cin >> T;
+    for (int cse=1; cse<=T; ++cse) {
+        char C[52][3];
+        for (int i=0; i<52; ++i)
+            cin >> C[i];
+
+        int top = 26, y=0;
+        for (int i=0; i<3; ++i) {
+            if (top < 0) throw 1;
+            int x = isdigit(C[top][0]) ? C[top][0]-'0' : 10;
+            y += x;
+            top -= 10-x+1;
+        }
+
+        cout << "Case " << cse << ": "
+             << (y<=top ? C[y] : C[25+y-top]) << endl;
+    }
+}
