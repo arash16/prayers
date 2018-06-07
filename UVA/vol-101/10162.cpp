4ee2c24524d715e49009680e05e79735ae890090
Arash <arash.shakery@gmail.com>
2018-06-07 22:15:37 +0430

    10162

---
+++ UVA/vol-101/10162.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int powmod(int a, int b, int c) {
+    if (!b) return 1;
+    int d = powmod(a, b>>1, c) % c;
+    d = (d*d) % c;
+    if (b&1) d *= a;
+    return d%c;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);
+
+    int X[201] = {}, n;
+    for (int i=1; i<=200; ++i)
+        X[i] = (X[i-1] + powmod(i, i, 10)) % 10;
+
+    string s;
+    while (cin >> s && s!="0") {
+        if (s.length() > 3)
+            s = s.substr(s.length()-3);
+
+        stringstream sin(s); sin >> n;
+        cout << X[n % 200] << endl;
+    }
+}
