25cf6b454f47171d4d2fd1ade4f9d517e14de3ec
Arash <arash.shakery@gmail.com>
2018-06-09 17:38:33 +0430

    10392

---
+++ UVA/vol-103/10392.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+bool isnp[1000143];
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);
+
+    vector<int> primes;
+    for (int i=2; i<1000143; ++i)
+        if (!isnp[i]) {
+            for (int j=i+i; j<1000143; j+=i)
+                isnp[j] = 1;
+            primes.push_back(i);
+        }
+
+    long long n;
+    while (cin >> n && n>=0) {
+        for (int p: primes) {
+            if (p > n) break;
+            while (n % p == 0) {
+                cout << "    " << p << '\n';
+                n /= p;
+            }
+        }
+        if (n > 1)
+            cout << "    " << n << '\n';
+        cout << '\n';
+    }
+}
