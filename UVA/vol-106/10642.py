f244b804320ad5d42fa15a6c29320e582cdf9573
Arash <arash.shakery@gmail.com>
2021-01-17 02:04:40 +0330

    solve 10642

---
+++ UVA/vol-106/10642.py
@@ -0,0 +1,11 @@
+from sys import stdin, stdout
+
+def num(x, y):
+  return (x+y)*(x+y+1)//2 + x + 1
+
+I = list(map(int, stdin.read().split()))
+for cse in range(1, I[0]+1):
+  s = 4 * cse - 3
+  r = num(I[s+2], I[s+3]) - num(I[s], I[s+1])
+  stdout.write('Case {}: {}\n'.format(cse, r))
+
