dc3d2c7c1cbf48bd5ee09a8baf331582c32cc919
Arash <arash.shakery@gmail.com>
2021-01-16 03:42:18 +0330

    solve 10014

---
+++ UVA/vol-100/10014.py
@@ -0,0 +1,17 @@
+from sys import stdin, stdout
+inp = stdin.read().split()
+
+s = 1
+T = int(inp[0])
+for cse in range(0, T):
+  if cse > 0: stdout.write('\n')
+
+  n = int(inp[s])
+  a = float(inp[s+1])
+  b = float(inp[s+2])
+  c = list(map(float, inp[s+3:s+3+n]))
+  s += n + 3
+
+  res = (n*a + b - sum(2 * (n-i) * c for i,c in enumerate(c))) / (n+1)
+  stdout.write('{:.2f}\n'.format(res))
+
