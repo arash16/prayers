302b92e5eab76de66f3a61bbcd2d1bccb99d0e31
Arash <arash.shakery@gmail.com>
2021-01-19 20:39:11 +0330

    solve 10213

---
+++ UVA/vol-102/10213.py
@@ -0,0 +1,7 @@
+from sys import stdin, stdout
+
+# f(n) = f(n-1) + sum( (i-1)*(n-1-i) , i=1..n-1 )
+for n in map(int, stdin.read().split()[1:]):
+  r = n*(n*(n*(n-6)+23)-18)//24 + 1
+  stdout.write('{}\n'.format(r))
+
