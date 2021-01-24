67332fd62462c985b08b5aee794056038c943437
Arash <arash.shakery@gmail.com>
2021-01-24 19:13:18 +0330

    solve 914

---
+++ UVA/vol-009/914.py
@@ -0,0 +1,42 @@
+from sys import stdin, stdout
+I = list(map(int, stdin.read().split()))
+
+primes = [2]
+maxP = 1000001
+isP = [True] * maxP
+isP[0] = isP[1] = False
+for i in range(3, maxP, 2):
+  if isP[i]:
+    primes.append(i)
+    for j in range(i*i, maxP, i):
+      isP[j] = False
+
+for c in range(0, I[0]):
+  [L, R] = I[2*c+1: 2*c+3]
+
+  l = -1
+  F = {}
+  mx = 0
+  mxi = -1
+  isc = False
+  for i, p in enumerate(primes):
+    if p > R: break
+    if p < L: continue
+    if l != -1:
+      dif = p - l
+      if not dif in F:
+        F[dif] = 1
+      else:
+        F[dif] += 1
+
+      if F[dif] > mx:
+        mx = F[dif]
+        mxi = dif
+        isc = True
+      elif F[dif] == mx and dif != mxi:
+        isc = False
+    l = p
+
+  if isc: stdout.write('The jumping champion is {}\n'.format(mxi))
+  else: stdout.write('No jumping champion\n')
+
