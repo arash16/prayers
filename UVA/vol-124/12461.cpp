ee6983138b743f17a23fb70d2250d57e403854b9
arash16 <arash.shakery@gmail.com>
2022-06-17 17:29:18 +0430

    solve 12461

---
+++ UVA/vol-124/12461.cpp
@@ -0,0 +1,23 @@
+/*
+  >>~~ UVa Online Judge ACM Problem Solution ~~<<
+
+  ID: 12461
+  Name: Airplane
+  Problem: https://onlinejudge.org/external/124/12461.pdf
+  Language: C++
+
+  Author: Arash Shakery
+  Email: arash.shakery@gmail.com
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(0);cin.tie(0);
+
+  int n;
+  while (cin >> n && (n)) {
+    cout << "1/2" << endl;
+  }
+}
